5cabf4d285d4e5a398c218bb8ea83950a8d99d33
Crazyvipa <celebritynudity@gmail.com>
2012-02-15 10:42:46 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd5bf5293a363b887d19262d7cd373860dbe325cb7823bacba7d8e5a74febafe1a9b33d974fb6d4c9b919798d1a1a1a94b75b3fa7b18f8adfdb955cebdd547e94'
